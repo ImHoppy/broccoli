3bdb1594de49e6071d8a46eb46e9aa05e8e5722e
Hoppy <dev.hoppy@gmail.com>
2023-12-28 21:52:16 +0000

    Update db to have steps images and fix error

--- scripts/seed.js
+++ scripts/seed.js
@@ -4,12 +4,13 @@ async function seedRecipes(client, dummy_data = false) {
 	try {
 		const createTable = await client.sql`
 			CREATE TABLE IF NOT EXISTS recipes (
-				id SERIAL DEFAULT PRIMARY KEY,
+				id SERIAL PRIMARY KEY,
 				title VARCHAR(255) NOT NULL,
-				images VARCHAR(255),
+				images VARCHAR(255)[],
+				steps_image VARCHAR(255)[],
 				tags VARCHAR(255)[],
 				ingredients VARCHAR(255)[],
-				desc TEXT,
+				description TEXT,
 				rating integer,
 				author VARCHAR(255),
 				created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -27,32 +28,32 @@ async function seedRecipes(client, dummy_data = false) {
 		const recipes = await Promise.all(
 			[
 				client.sql`
-					INSERT INTO recipes (title, images, tags, ingredients, desc, rating, author)
-					VALUES ('Pasta', '', '{"pasta", "italian"}', '{"250g pasta", "canned tomatoes", "cheese"}', '1. Cook pasta\n2. Add tomato sauce\n3. Add cheese\n4. Enjoy!', 4, 'John Doe')
+					INSERT INTO recipes (title, images, tags, ingredients, description, rating, author)
+					VALUES ('Pasta', '{}', '{"pasta", "italian"}', '{"250g pasta", "canned tomatoes", "cheese"}', '1. Cook pasta\n2. Add tomato sauce\n3. Add cheese\n4. Enjoy!', 4, 'John Doe')
 				`,
 				client.sql`
-					INSERT INTO recipes (title, images, tags, ingredients, desc, rating, author)
-					VALUES ('Pizza', '', '{"pizza", "italian"}', '{"pizza dough", "tomato base", "cheese"}', '1. Roll out dough\n2. Add tomato sauce\n3. Add cheese\n4. Bake for 20 minutes\n5. Enjoy!', 5, 'John Doe')
+					INSERT INTO recipes (title, images, tags, ingredients, description, rating, author)
+					VALUES ('Pizza', '{}', '{"pizza", "italian"}', '{"pizza dough", "tomato base", "cheese"}', '1. Roll out dough\n2. Add tomato sauce\n3. Add cheese\n4. Bake for 20 minutes\n5. Enjoy!', 5, 'John Doe')
 				`,
 				client.sql`
-					INSERT INTO recipes (title, images, tags, ingredients, desc, rating, author)
-					VALUES ('Burger', '', '{"burger", "american"}', '{"buns", "patty", "cheese", "lettuce", "tomato"}', '1. Cook patty\n2. Add cheese\n3. Add lettuce and tomato\n4. Assemble burger\n5. Enjoy!', 3, 'John Doe')
+					INSERT INTO recipes (title, images, tags, ingredients, description, rating, author)
+					VALUES ('Burger', '{}', '{"burger", "american"}', '{"buns", "patty", "cheese", "lettuce", "tomato"}', '1. Cook patty\n2. Add cheese\n3. Add lettuce and tomato\n4. Assemble burger\n5. Enjoy!', 3, 'John Doe')
 				`,
 				client.sql`
-					INSERT INTO recipes (title, images, tags, ingredients, desc, rating, author)
-					VALUES ('Chocolate Cake', '', '{"desert", "cake", "chocolate"}', '{"80g butter", "1 teaspoon of yeast", "60g of flour", "100g sugar", "150g of pastry chocolate", "3 eggs", "50g ground almonds"}','1. Mix ingredients\n2. Bake for 30 minutes\n3. Enjoy!', 5, 'John Doe')
+					INSERT INTO recipes (title, images, tags, ingredients, description, rating, author)
+					VALUES ('Chocolate Cake', '{"https://i.imgur.com/tuReSuq.jpeg"}', '{"desert", "cake", "chocolate"}', '{"80g butter", "1 teaspoon of yeast", "60g of flour", "100g sugar", "150g of pastry chocolate", "3 eggs", "50g ground almonds"}','1. Mix ingredients\n2. Bake for 30 minutes\n3. Enjoy!', 5, 'John Doe')
 				`,
 			]
 		);					
 
-		console.log(`Seeded ${recipes.length} users`);
+		console.log(`Seeded ${recipes.length} recipes`);
 
 		return {
 			createTable,
 			recipes,
 		};
 	} catch (error) {
-		console.error('Error seeding users:', error);
+		console.error('Error seeding recipes:', error);
 		throw error;
 	}
 }
@@ -60,7 +61,7 @@ async function seedRecipes(client, dummy_data = false) {
 async function main() {
 	const client = await db.connect();
 
-	await seedRecipes(client, false);
+	await seedRecipes(client, true);
 
 	await client.end();
 }
